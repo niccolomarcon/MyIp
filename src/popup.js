aa7b22f9f6599e2c624366e9e7543880cba7f5ae
Niccolò Marcon <marcon.niccolo@gmail.com>
2016-01-28 16:31:10 +0100

    first try. not working

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,38 @@
-var input = document.createElement('textarea');
-document.body.appendChild(input);
-input.value = 'ciaone';
-input.focus();
-input.select();
-document.execCommand('Copy');
-input.remove();
+function copy(ip) {
+  var input = document.createElement('textarea');
+  document.body.appendChild(input);
+  input.value = ip;
+  input.focus();
+  input.select();
+  document.execCommand('Copy');
+  input.remove();
+}
+
+function print(text) {
+  document.getElementById('ip').innerHTML = text;
+}
+
+function getIp() {
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://checkip.dyndns.org', true);
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400) {
+      var resp = request.responseText;
+      var re = new RegExp('s/[a-zA-Z/<> :]//g');
+      var ip = re.exec(resp);
+      print(ip);
+      copy(ip);
+    } else {
+      print('Error from the server');
+    }
+  };
+
+  request.onerror = function() {
+    print('Unable to connect');
+  };
+
+  request.send();
+}
+
+getIp();
