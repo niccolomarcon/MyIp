641af8d3bfbf10ff6f993992d1ad88048d26bb24
Niccolò Marcon <marcon.niccolo@gmail.com>
2016-01-28 21:51:20 +0100

    test fot getting the ip. clipboard works

--- src/popup.js
+++ src/popup.js
@@ -1,38 +1,14 @@
-function copy(ip) {
-  var input = document.createElement('textarea');
-  document.body.appendChild(input);
-  input.value = ip;
-  input.focus();
-  input.select();
-  document.execCommand('Copy');
-  input.remove();
-}
+console.log(ip)
+// Coping the ip into the clipboard
+var input = document.createElement('textarea');
+document.body.appendChild(input);
+input.value = ip;
+input.focus();
+input.select();
+document.execCommand('Copy');
+input.remove();
 
-function print(text) {
-  document.getElementById('ip').innerHTML = text;
-}
+// Display the ip
+document.getElementById('ip').innerHTML = ip;
 
-function getIp() {
-  var request = new XMLHttpRequest();
-  request.open('GET', 'http://checkip.dyndns.org', true);
 
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      var resp = request.responseText;
-      var re = new RegExp('s/[a-zA-Z/<> :]//g');
-      var ip = re.exec(resp);
-      print(ip);
-      copy(ip);
-    } else {
-      print('Error from the server');
-    }
-  };
-
-  request.onerror = function() {
-    print('Unable to connect');
-  };
-
-  request.send();
-}
-
-getIp();
